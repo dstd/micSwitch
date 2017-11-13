b42705d480c7c91750387409fdb6ae16edc8c422
Denis Stanishevskiy <std.denis@gmail.com>
2017-11-13 13:06:54 +0300

    Initial Commit

---
+++ micSwitch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  micSwitch
+//
+//  Created by dstd on 13/11/2017.
+//  Copyright © 2017 denis-st. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
